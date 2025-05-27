b1a55aec7b066e7bf9835de544988f2b8d69ec9a
kavinduabc <piumalranepura2002@gmail.com>
2025-05-27 07:46:57 +0530

    create the folder for services

--- lib/pages/onbaording.dart
+++ lib/pages/onbaording.dart
@@ -12,11 +12,15 @@ class _OnboardingState extends State<Onboarding>{
    @override
    Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
+        margin:EdgeInsets.only(top: 40.0) ,
         child: Column(
           children: [
             Image.asset("images/onboard.png"),
+            Text("The Fastest \n Food Delivery"),
           ],
+          
         ),
       ),
     );
